3c40bb26c31f703c536a3c89acd9ae2b0e090fc0
radu.pana <radu-mihai.pana-talpeanu@chase.io>
2024-06-30 13:19:23 +0100

    Print instance details

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-01b799c439fd5516a"
   instance_type = "t2.micro"
 }
+
+output "instance_id" {
+  value = aws_instance.my_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
